1148a2feef7e3f08d01a4bbba232031e0eff4111
Robert Dyro <robert.dyro@gmail.com>
2021-11-23 18:26:58 -0800

    pushing first working version

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+# borrowed from https://pythonhosted.org/an_example_pypi_project/setuptools.html
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="jfi",
+    version="0.1.0",
+    author="Robert Dyro",
+    description=("Simplified and user friendly interface to JAX."),
+    license="MIT",
+    packages=["jfi"],
+    long_description=read("README.md"),
+)
